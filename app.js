ac19230c845caace09d32f01bfaefe5511b4d4da
Hongbin Li <hblee12294@gmail.com>
2018-09-06 20:36:42 -0700

    add: basic mesh and line demo

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'))
+
+// router
 app.get('/', (req, res) => res.send('demo start'))
 
-app.listen(5000, () => console.log('App listening on port 5000 ...'))
+app.listen(5000, () => console.log('App listening on http://localhost:5000 ...'))
